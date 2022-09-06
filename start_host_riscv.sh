bea8e7d1d75f834cec3ba46fac4a41db200a0367
Dylan Reid <dgreid@rivosinc.com>
2022-09-06 14:53:15 -0700

    wip
    Change-Id: If4268ee83179b69105cc14fe8d26b6f2d935da88

--- start_host_riscv.sh
+++ start_host_riscv.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-~/src/qemu/build/qemu-system-riscv64 -kernel ~/src/linux/arch/riscv/boot/Image -nographic -machine virt,aia=aplic-imsic,aia-guests=4 -m 4G  -append "root=/dev/vda ro console=ttyS0 earlycon=sbi keep_bootcon bootmem_debug"                 -bios ~/src/opensbi/build/platform/generic/firmware/fw_jump.bin                 -drive file=/home/dylan/src/buildroot/output/images/rootfs.ext2,format=raw,id=hd0                 -device virtio-blk-device,drive=hd0                 -virtfs local,path=./,mount_tag=host,security_model=none  -netdev user,id=network0,hostfwd=tcp::1234-:1234  -device e1000,netdev=network0,mac=52:54:00:54:46:69
+qemu-system-riscv64 \
+	-kernel /scratch/linux/arch/riscv/boot/Image \
+	-nographic \
+	-machine virt,aia=aplic-imsic,aia-guests=4 \
+	-cpu rv64,x-aia=true \
+	-m 4G  \
+	-smp 1 \
+	-append "root=/dev/vda ro console=ttyS0 "                        \
+	-drive file=/scratch/buildroot/output/images/rootfs.ext2,format=raw,id=hd0                 \
+	-device virtio-blk-device,drive=hd0                
+
+#-virtfs local,path=./,mount_tag=host,security_model=none  -netdev user,id=network0,hostfwd=tcp::1234-:1234  -device e1000,netdev=network0,mac=52:54:00:54:46:69
 
