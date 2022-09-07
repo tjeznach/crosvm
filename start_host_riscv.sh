a13a0bc866012bf39322f4765e086e040f4bcfa0
Dylan Reid <dgreid@rivosinc.com>
2022-09-07 15:57:31 -0700

    wip
    Change-Id: Iee8711c387e343c96de134ca4d30180e0214ce31

--- start_host_riscv.sh
+++ start_host_riscv.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-qemu-system-riscv64 \
+/scratch/qemu/build/qemu-system-riscv64 \
 	-kernel /scratch/linux/arch/riscv/boot/Image \
 	-nographic \
 	-machine virt,aia=aplic-imsic,aia-guests=4 \
-	-cpu rv64,x-aia=true \
+	-cpu rv64 \
 	-m 4G  \
-	-smp 1 \
-	-append "root=/dev/vda ro console=ttyS0 "                        \
+	-smp 2 \
+	-append "root=/dev/vda ro console=hvc0 earlycon=sbi"                        \
 	-drive file=/scratch/buildroot/output/images/rootfs.ext2,format=raw,id=hd0                 \
-	-device virtio-blk-device,drive=hd0                
-
-#-virtfs local,path=./,mount_tag=host,security_model=none  -netdev user,id=network0,hostfwd=tcp::1234-:1234  -device e1000,netdev=network0,mac=52:54:00:54:46:69
-
+	-device virtio-blk-device,drive=hd0                 \
+	-virtfs local,path=./,mount_tag=host,security_model=none \
+	-netdev user,id=network0,hostfwd=tcp::1234-:1234 \
+	-device e1000,netdev=network0,mac=52:54:00:54:46:69
