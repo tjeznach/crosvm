5f7705138a2b57468ce3a194032c36bdd40f805f
Dylan Reid <dgreid@rivosinc.com>
2022-09-28 16:19:52 -0700

    squash with x86_64.rs
    Change-Id: I8de242a3428e1dffdf03c02039fc16e3701bb5db

--- x86_64/src/lib.rs
+++ x86_64/src/lib.rs
@@ -893,7 +893,6 @@ impl arch::LinuxArch for X8664arch {
         num_cpus: usize,
         _has_bios: bool,
         cpu_config: Option<CpuConfigX86_64>,
-        _fdt_address: Option<GuestAddress>,
     ) -> Result<()> {
         let cpu_config = match cpu_config {
             Some(config) => config,
